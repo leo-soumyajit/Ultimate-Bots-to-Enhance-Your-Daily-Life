3fa66fa86141fe0bd19d3eee804deacd414099ed
Soumyajit Banerjee <banerjeesoumyajit2005@gmail.com>
2025-02-20 22:00:08 +0530

    Webconfig updated

--- src/main/java/com/soumyajit/Spring/Ai/Project/appConfig/WebConfig.java
+++ src/main/java/com/soumyajit/Spring/Ai/Project/appConfig/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:63342")
+                .allowedOrigins("http://localhost:63828")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
